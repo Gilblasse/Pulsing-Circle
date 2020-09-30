e5a5ee6699d34c1e36184ce2d102af9cd663c1b0
Gilblasse <nethelbert.blasse@gmail.com>
2020-09-30 16:17:33 -0400

    Tested circle positional prop

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
 
   return (
     <div>
-      <PulseCircle radius={20} color='blue' thickness={3} top={22} left={23}/>
+      <PulseCircle radius={20} color='blue' thickness={3} top={2} left={23}/>
       <PulseCircle loopType='yoyo'/>
     </div>
   );
